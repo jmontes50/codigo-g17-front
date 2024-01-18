3e5cebd2efe2a838663aaa73c30a694dcdfcf4cd
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-01-17 22:17:09 -0500

    primera app todo

--- semana7/02-todo.js
+++ semana7/02-todo.js
@@ -6,11 +6,15 @@
 
 let inputTarea = document.getElementById('idTarea');
 let btnAgregar = document.querySelector('#btnAgregar');
+let ulTareas = document.getElementById('tareas');
 
 console.log("inputTarea", inputTarea);
 console.log("btnAgregar", btnAgregar);
 
 let arrTareas = [];
+
+let header = document.querySelector('header');
+// console.log(typeof header.innerHTML)
 
 /**
  * Historias de usuario
@@ -21,5 +25,15 @@ btnAgregar.addEventListener("click", function(){
     // console.log(inputTarea.value)
     arrTareas.push(inputTarea.value);
     // console.log(arrTareas)
-    console.table(arrTareas)
-})
+    // console.table(arrTareas)
+    let htmlTareas = "";
+    //vamos a dibujar las tareas dentro del UL
+    for(let i = 0; i < arrTareas.length; i++){
+        // console.log(arrTareas[i]);
+        htmlTareas = htmlTareas + `<li>${arrTareas[i]}</li>`;
+    }
+    //al tener el HTML en formato de string dentro de htmlTareas, podemos utilizarlo para agregarlo a la propiedad innerHTML de ulTareas
+    ulTareas.innerHTML = htmlTareas;
+    //limpiamos el value de input
+    inputTarea.value = "";
+});
