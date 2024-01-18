8ddfb38cfed805078ab4292d6c255c2358bcd6c9
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-01-17 20:53:25 -0500

    added setAttribute

--- semana7/01-dom.js
+++ semana7/01-dom.js
@@ -62,8 +62,49 @@ const itemsLista = document.querySelectorAll('.listItem');
 
 console.log("itemsLista", itemsLista)
 
+/**función Callback, estamos pasando una función como parámetro */
 itemsLista.forEach(function(item){
     console.log("LI - ", item)
     item.style.color = "steelblue";
     item.style.fontFamily = "Courier"
 })
+
+//Eventos
+//para poder escuchar eventos vamos a necesitar:
+//TODOS los elementos, sea un boton, div, input van a tener eventos
+
+console.log("Listener", miBoton.addEventListener)
+
+let miInput = document.getElementById('inputEmail');
+
+console.log("inputEmail", miInput)
+/**Si yo deseo interactuar con el HTML:
+ * 1. voy a necesitar referencias, variables a los elementos
+ * que esten presentes
+ * 2. a partir de ahi puedo utilizar propiedades, métodos
+ * ej. innerHTML, style.color, addEventListener
+ * 3. según sea método o propiedad la puedo utilizar para agregar dinamismo a mi app
+ */
+/**elemento.addEventListener("nombreDelEvento", funcion) */
+miBoton.addEventListener("click", function(){
+    //alert abre un ventana con un mensaje
+    // alert("Hiciste click en el botón")
+    console.log("Hola!")
+    console.log("input texto", miInput.value)
+    //Todo input va a tener una propiedad que se llama value
+    /**El método includes() determina si una cadena de texto puede ser encontrada dentro de otra cadena de texto, devolviendo true o false según corresponda. */
+    if(miInput.value.includes(".com")){
+        console.log("Existeeee!!!")
+    }else{
+        console.log("No hayyyyyyyyy")
+    }
+})
+
+let img = document.querySelector('#imagen');
+
+console.log(img.getAttribute("src"))
+//setAttribute recibe dos parámetros
+img.setAttribute("src", "https://images.unsplash.com/photo-1616011224441-f652db413327?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FtaW9uZXN8ZW58MHx8MHx8fDA%3D")
+
+img.setAttribute("alt", "Camión San Patricio")
+
