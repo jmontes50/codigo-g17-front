7477363e0964830f0ddfc69d7c55aa3b89563af2
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-01-17 22:28:27 -0500

    refactorización de la app

--- semana7/02-todo.js
+++ semana7/02-todo.js
@@ -20,19 +20,38 @@ let header = document.querySelector('header');
  * Historias de usuario
  * Como usuario quiero que la aplicación me permita escribir una tarea para poder listarla.
  */
+
+function generarHTMLTareas(tareas){
+    let htmlTareas = "";
+    //vamos a dibujar las tareas dentro del UL
+    for(let i = 0; i < tareas.length; i++){
+        // console.log(tareas[i]);
+        htmlTareas = htmlTareas + `<li>${tareas[i]}</li>`;
+    }
+    return htmlTareas;
+}
+
 //elemento.addEventListener("nombredelevento", funcion)
 btnAgregar.addEventListener("click", function(){
     // console.log(inputTarea.value)
     arrTareas.push(inputTarea.value);
     // console.log(arrTareas)
     // console.table(arrTareas)
+
+    //VERSION 1
+    /*
     let htmlTareas = "";
     //vamos a dibujar las tareas dentro del UL
     for(let i = 0; i < arrTareas.length; i++){
         // console.log(arrTareas[i]);
         htmlTareas = htmlTareas + `<li>${arrTareas[i]}</li>`;
     }
+    */
     //al tener el HTML en formato de string dentro de htmlTareas, podemos utilizarlo para agregarlo a la propiedad innerHTML de ulTareas
+    
+   //VERSION 2
+    let htmlTareas = generarHTMLTareas(arrTareas)
+    console.log(htmlTareas)
     ulTareas.innerHTML = htmlTareas;
     //limpiamos el value de input
     inputTarea.value = "";
