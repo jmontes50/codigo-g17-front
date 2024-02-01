70931126e33b290443c410f891749d17da88225d
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-01-31 20:48:18 -0500

    hola mundo en react

---
+++ semana9/01-react/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css"
+//cuando importamos algo de JS o JSX, no es necesario agregarle la extensión, si es algo diferente si es imperativo
+import Mensaje from "./components/Mensaje"; 
+//un componente no es más que una función
+//El nombre de la función que representa el componente
+//1. El nombre tiene que estar en Mayúsculas
+function App (){
+  const titulo = "Mi primera app";
+  //3. esta función/componente Tiene que retornar un JSX
+  return (
+    //4 solamente podemos retornar UN solo elemento de JSX
+    <>
+      <h1>{titulo}</h1>
+      <section>
+        <h2 className="red">Holaaaaaaaaaaa</h2>
+        {/* Siempre que deseemos poner codigo de JS dentro de JSX voy a tener que abrir {} y adentro poner una expresión de JS*/}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsum id minima impedit numquam debitis qui. Resultado: {2 + 6}
+        </p>
+        <div>
+          <label htmlFor="idNombre">Nombre:</label>
+          <input type="text" id="idNombre" />
+        </div>
+      </section>
+      {/* Toda etiqueta ya sea de html o de un componente debe estar cerrada o con su etiqueta de cierre */}
+      <hr />
+      <Mensaje />
+      <Mensaje />
+      <Mensaje />
+    </>
+  )
+}
+
+//2. exportar esta función
+export default App;
---
+++ semana9/01-react/src/components/Mensaje.jsx
@@ -0,0 +1,18 @@
+//Ya no es obligatorio en importar React en c/componente
+//En algunas versiones de librerias como 
+//react Native si es necesario
+//import React from "react"; 
+const Mensaje = () => {
+    return (
+        // estas llaves de html que aparentan ser vacias
+        // se conocen como Fragment, funciona como un 
+        // contenedor pero que no se va a renderizar
+        <>
+            <p>
+                Mensaje: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet commodi porro, sint repellat reiciendis non earum fugiat rerum velit in animi vitae incidunt placeat nisi! Sapiente amet est fugiat architecto.
+            </p>
+        </>
+    )
+}
+
+export default Mensaje;
