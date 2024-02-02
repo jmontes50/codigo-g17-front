9711d5c997a07d16919fbca4e132a16ce9197e11
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-02-01 21:49:26 -0500

    renderizado de listas

--- semana9/03-useEffect/src/Renderizado.jsx
+++ semana9/03-useEffect/src/Renderizado.jsx
@@ -1,14 +1,49 @@
 import { useState } from "react";
 
 export default function Renderizado() {
-  const [cargando, setCargando] = useState(false);
+  const [cargando, setCargando] = useState(true);
   //operador ternario, es como un if corto
   //expresion a evaluar ? valorSiEsVerdadero : "valorSiEsFalso"
   //10 > 5 ? "verdadero" : "falso"
+  const paises = ["Perú","Chile","Colombia"];
+
+  const postresPeruanos = [
+    {
+      id: 1,
+      nombre: "Suspiro a la Limeña",
+      descripcion: "Dulce cremoso con merengue, decorado con canela",
+    },
+    {
+      id: 2,
+      nombre: "Mazamorra Morada",
+      descripcion: "Postre a base de maíz morado, frutas y camote",
+    },
+    {
+      id: 3,
+      nombre: "Picarones",
+      descripcion: "Masitas fritas de harina de trigo y camote, bañadas en miel de chancaca",
+    }
+  ];
+
   return (
     <>
       {/* renderizado condicional */}
       {cargando ? <p>Espere por favor..</p> : <p>Proceso terminado</p>}
+      <button onClick={() => {setCargando(!cargando)}}>
+        Cambiar
+      </button>
+      <hr />
+      <ul>
+        {/* renderizado de listas */}
+      {paises.map((pais, indice) => (<li key={indice}>{pais}</li>))}
+      </ul>
+      <hr />
+      <h2>Postres</h2>
+      {postresPeruanos.map((item) => (
+      <div key={item.id}>
+        <h3>{item.nombre}</h3>
+        <p>{item.descripcion}</p>
+      </div>))}
     </>
   );
 }
