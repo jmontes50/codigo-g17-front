bd74fc7e376c12fe5329c30b0284183425e2637f
Jorge Montesinos <osmarmontes50@gmail.com>
2024-02-08 20:35:16 -0500

    fix error selectcolors

--- semana9/administrador/src/components/SelectColors.jsx
+++ semana9/administrador/src/components/SelectColors.jsx
@@ -2,7 +2,18 @@ import { useState } from "react"
 import ShowColors from "./ShowColors";
 
 export default function SelectColors({ colors, handleColor }) {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("#000000");
+
+  const handleInputColor = () => {
+    //Truthy y Falsy
+    //Falsy false, null, undefined, 0, '', NaN
+    //si es que no he seleccionado un color
+    if (!color) {
+      alert("Por favor ingresa un color");
+      return;
+    } //pero si ya seleccione un color
+    handleColor(color);
+  }
 
   return (
     <div className="mb-4">
@@ -15,7 +26,7 @@ export default function SelectColors({ colors, handleColor }) {
         />
         <button
           type="button"
-          onClick={() => handleColor(color)}
+          onClick={handleInputColor}
           className="btn btn-dark"
         >
           Agregar Color
--- semana9/administrador/src/views/ManageProduct.jsx
+++ semana9/administrador/src/views/ManageProduct.jsx
@@ -1,4 +1,7 @@
+//import el orden a utilizar recomendado es cosas de react, librerias, componentes y funciones propias
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import { saveProduct } from "../services/productService";
 import { uploadFile } from "../services/storageService";
 import Cargando from "../components/Cargando";
@@ -10,22 +13,22 @@ export default function ManageProduct() {
   const [form, setForm] = useState({
     nombre: "",
     descripcion: "",
-    precio:0,
-    imagen:"https://loremflickr.com/640/480/clothes",
-    color:[],
-    stock:0,
-    review:[]
+    precio: 0,
+    imagen: "https://loremflickr.com/640/480/clothes",
+    color: [],
+    stock: 0,
+    review: []
   });
 
-
-
   const [estaCargando, setEstaCargando] = useState(false)
+
+  const navigate = useNavigate();
 
   const changeForm = (evento) => {
     // console.log("NAME", evento.target.name)
     const copyStateForm = {
       ...form, //Spread Operator
-      [evento.target.name]:evento.target.value
+      [evento.target.name]: evento.target.value
     }
     // console.log("COPYSTATEFORM", copyStateForm)
     setForm(copyStateForm)
@@ -40,124 +43,134 @@ export default function ManageProduct() {
     setEstaCargando(true)
     // console.log({ form })
     uploadFile(imagenProducto, "photos")
-    .then(urlImagen => {
-      // console.log(urlImagen)
-      return saveProduct({...form, imagen: urlImagen})
-    })
-    .then(() => {
-      setEstaCargando(false)
-      alert(`Se creo el producto ${form.nombre}`)
-    })
-    .catch(err => {
-      console.error(err)
-    })
+      .then(urlImagen => {
+        // console.log(urlImagen)
+        return saveProduct({ ...form, imagen: urlImagen });
+      })
+      .then(() => {
+        setEstaCargando(false)
+        // alert(`Se creo el producto ${form.nombre}`)
+        return Swal.fire({
+          icon: "success",
+          title: "Producto creado",
+          text: `Se creo el producto ${form.nombre}`
+        });
+      })
+      //aquí podriamos capturar el resultado de dar click al botón de confirmación
+      .then(() => {
+        //navigate(indicamos como string a que ruta queremos ir)
+        navigate('/');
+      })
+      .catch(err => {
+        console.error(err);
+      })
   }
 
   const handleColor = (newColor) => {
     // console.log("form.color", [...form.color] )
     //  console.log({newColor})
-    setForm({...form, color:[...form.color, newColor]})
+    setForm({ ...form, color: [...form.color, newColor] })
   }
 
-  if(estaCargando) {
+  if (estaCargando) {
     return <Cargando />
   }
 
   return (
     <main className="container p-4">
-        <h2>Crear Producto</h2>
-        <form>
-            <div className="mb-3">
-                <label 
-                    className="form-label" 
-                    htmlFor="inputNombre"
-                >
-                    Nombre producto:
-                </label>
-                <input 
-                    type="text"
-                    placeholder="Camisa Negra"
-                    id="inputNombre"
-                    className="form-control"
-                    value={form.nombre}
-                    name="nombre"
-                    onChange={(evento) => {changeForm(evento)}}
-                />
-            </div>
-            <div className="mb-3">
-                <label 
-                    className="form-label" 
-                    htmlFor="inputDescripcion"
-                >
-                    Descripción producto:
-                </label>
-                <input 
-                    type="text"
-                    placeholder="Camisa Negra de Tela texturada"
-                    id="inputDescripcion"
-                    className="form-control"
-                    value={form.descripcion}
-                    name="descripcion"
-                    onChange={(evento) => {changeForm(evento)}}
-                />
-            </div>
-            <div className="mb-3">
-                <label 
-                    className="form-label" 
-                    htmlFor="inputPrecio"
-                >
-                    Precio producto:
-                </label>
-                <input 
-                    type="number"
-                    placeholder="100"
-                    id="inputPrecio"
-                    className="form-control"
-                    value={form.precio}
-                    name="precio"
-                    onChange={(evento) => {changeForm(evento)}}
-                />
-            </div>
-            <div className="mb-3">
-                <label 
-                    className="form-label" 
-                    htmlFor="inputImagen"
-                >
-                    Imagen a guardar
-                </label>
-                <input
-                  id="inputImagen"
-                  type="file"
-                  className="form-control"
-                  onChange={handleImage}
-                />
-            </div>
-            <div className="mb-3">
-                <label 
-                    className="form-label" 
-                    htmlFor="inputStock"
-                >
-                    Stock producto:
-                </label>
-                <input 
-                    type="number"
-                    placeholder="150"
-                    id="inputStock"
-                    className="form-control"
-                    value={form.stock}
-                    name="stock"
-                    onChange={(evento) => {changeForm(evento)}}
-                />
-            </div>
-            <SelectColors colors={form.color} handleColor={handleColor}/>
-            <button
-              className="btn btn-primary btn-lg"
-              type="button"
-              onClick={handleCreate}
-            >
-              Guardar
-            </button>
-        </form>
+      <h2>Crear Producto</h2>
+      <form>
+        <div className="mb-3">
+          <label
+            className="form-label"
+            htmlFor="inputNombre"
+          >
+            Nombre producto:
+          </label>
+          <input
+            type="text"
+            placeholder="Camisa Negra"
+            id="inputNombre"
+            className="form-control"
+            value={form.nombre}
+            name="nombre"
+            onChange={(evento) => { changeForm(evento) }}
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            className="form-label"
+            htmlFor="inputDescripcion"
+          >
+            Descripción producto:
+          </label>
+          <input
+            type="text"
+            placeholder="Camisa Negra de Tela texturada"
+            id="inputDescripcion"
+            className="form-control"
+            value={form.descripcion}
+            name="descripcion"
+            onChange={(evento) => { changeForm(evento) }}
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            className="form-label"
+            htmlFor="inputPrecio"
+          >
+            Precio producto:
+          </label>
+          <input
+            type="number"
+            placeholder="100"
+            id="inputPrecio"
+            className="form-control"
+            value={form.precio}
+            name="precio"
+            onChange={(evento) => { changeForm(evento) }}
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            className="form-label"
+            htmlFor="inputImagen"
+          >
+            Imagen a guardar
+          </label>
+          <input
+            id="inputImagen"
+            type="file"
+            className="form-control"
+            onChange={handleImage}
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            className="form-label"
+            htmlFor="inputStock"
+          >
+            Stock producto:
+          </label>
+          <input
+            type="number"
+            placeholder="150"
+            id="inputStock"
+            className="form-control"
+            value={form.stock}
+            name="stock"
+            onChange={(evento) => { changeForm(evento) }}
+          />
+        </div>
+        <SelectColors colors={form.color} handleColor={handleColor} />
+        <button
+          className="btn btn-primary btn-lg"
+          type="button"
+          onClick={handleCreate}
+        >
+          Guardar
+        </button>
+      </form>
     </main>
   )
 }
