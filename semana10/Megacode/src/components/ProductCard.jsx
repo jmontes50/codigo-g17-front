6f78ac4872e81e94f59b700fdbb66f37ea455873
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-02-15 20:32:31 -0500

    added vista detalle

--- semana10/Megacode/src/components/ProductCard.jsx
+++ semana10/Megacode/src/components/ProductCard.jsx
@@ -1,24 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function ProductCard({
-  product: { nombre, precio, stock, imagen }
+  product: { nombre, precio, stock, imagen, id },
 }) {
   return (
     // card
-    <div className="bg-gray-200 p-4 rounded-md">
-      {/* parte superior, la imagen */}
-      <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-lg relative">
-        <img
-          src={imagen}
-          className="w-full h-64 object-cover"
-          alt={nombre}
-        />
-        <div className="absolute top-0 right-0 bg-blue-500 text-white p-2">
-          {stock} Unid.
+    <Link to={`/detail/${id}`}>
+      <div className="bg-gray-200 p-4 rounded-md">
+        {/* parte superior, la imagen */}
+        <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-lg relative">
+          <img src={imagen} className="w-full h-64 object-cover" alt={nombre} />
+          <div className="absolute top-0 right-0 bg-blue-500 text-white p-2">
+            {stock} Unid.
+          </div>
+        </div>
+        <div className="p-4">
+          <h4 className="text-md font-semibold mb-2">{nombre}</h4>
+          <p className="text-sm text-gray-600">Precio: S/ {precio}</p>
         </div>
       </div>
-      <div className="p-4">
-        <h4 className="text-md font-semibold mb-2">{nombre}</h4>
-        <p className="text-sm text-gray-600">Precio: S/ {precio}</p>
-      </div>
-    </div>
-  )
+    </Link>
+  );
 }
--- semana10/Megacode/src/views/ProductDetail.jsx
+++ semana10/Megacode/src/views/ProductDetail.jsx
@@ -1,7 +1,43 @@
-
+import { useParams } from "react-router-dom";
+import useData from "../hooks/useAxios";
 
 export default function ProductDetail() {
-    return (
-        <div>ProductDetail</div>
-    )
+  const { id } = useParams();
+
+  const { data, error, loading } = useData(
+    `${import.meta.env.VITE_ENDPOINT_BASE}/productos/${id}`
+  );
+
+  console.log(data);
+
+  console.log(id);
+  return (
+    <div className="w-75 mx-auto py-6 grid grid-cols-1 md:grid-cols-2">
+      <img src={data.imagen} alt={data.nombre} className="w-full" />
+      <div className="p-4">
+        <h2 className="text-2xl font-semibold mb-2">{data.nombre}</h2>
+        <div className="grid grid-cols-2 my-5">
+          <span className="text-gray-600">
+            SKU:<b className="font-semibold text-black">######</b>
+          </span>
+          <span className="text-gray-600">
+            Disponibilidad:
+            <b className="font-semibold text-green-500">Disponible</b>
+          </span>
+          <span className="text-gray-600">
+            Marca:
+            <b className="font-semibold text-black">Estándar</b>
+          </span>
+          <span className="text-gray-600">
+            Categoría:
+            <b className="font-semibold text-black">Ropa</b>
+          </span>
+        </div>
+        <h3>Descripción:</h3>
+        <p className="text-gray-600">{data.descripcion}</p>
+        <p className="text-blue-600 text-xl">Precio: S/ {data.precio}</p>
+        <p className="text-blue-600 text-xl">Stock: {data.stock}</p>
+      </div>
+    </div>
+  );
 }
