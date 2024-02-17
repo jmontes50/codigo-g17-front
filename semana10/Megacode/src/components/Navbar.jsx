6b3189e1e1bf073010dd057f1a20d8a6ff5b95f8
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-02-16 20:22:08 -0500

    improve navbar 2

--- semana10/Megacode/src/components/Navbar.jsx
+++ semana10/Megacode/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { CartContext } from "../context/cartContext";
 
 export default function Navbar() {
-
   const { totalCart } = useContext(CartContext);
 
   return (
@@ -36,46 +35,55 @@ export default function Navbar() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {/* items */}
-                      <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white,
+                    <Link
+                      to="/"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white,
                           rounded-md px-3 py-2 text-sm font-medium"
-                      >
-                        Home
-                      </Link>
-                      <Link to="/products" className="text-gray-300 hover:bg-gray-700 hover:text-white,
+                    >
+                      Home
+                    </Link>
+                    <Link
+                      to="/products"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white,
                           rounded-md px-3 py-2 text-sm font-medium"
-                      >
-                        Productos
-                      </Link>
+                    >
+                      Productos
+                    </Link>
                   </div>
                 </div>
               </div>
+              {/* derecha */}
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link 
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none"
+                  >
+                  <i className="fa-solid fa-cart-shopping text-2xl text-gray-300"></i>
+                  {totalCart}
+                </Link>
+              </div>
             </div>
           </div>
+          {/*  */}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              <Link
+                to="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white, block 
+                          rounded-md px-3 py-2 text-sm font-medium"
+              >
+                Home
+              </Link>
+              <Link
+                to="/products"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white, block 
+                          rounded-md px-3 py-2 text-sm font-medium"
+              >
+                Productos
+              </Link>
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
-
-
-
-    /*
-    <nav className="bg-blue-700 p-4">
-      <div className="flex justify-between items-center">
-        <div className="container mx-auto">
-          <h1 className="text-white text-2xl font-bold">MegaCode</h1>
-          <div className="flex space-x-4">
-            <Link to="/" className="text-white">
-              Home
-            </Link>
-            <Link to="/products" className="text-white">
-              Productos
-            </Link>
-          </div>
-        </div>
-
-        <div className="flex space-x-4 text-white">Carrito: {totalCart}</div>
-      </div>
-    </nav>
-    */
   );
-  
 }
