d43a3167c21615ce92098a91fc4c05069bf33d77
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-03-01 22:41:04 -0500

    added redux

--- semana13/firechat/src/App.jsx
+++ semana13/firechat/src/App.jsx
@@ -1,8 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { setUser, resetUser } from "./app/auth/authSlice";
+import { auth } from "./firebase/config";
 import Sign from "./views/Sign";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const infoUser = {
+          name: user.displayName,
+          logged: true,
+          uid: user.uid,
+          photo: user.photoURL,
+        };
+        dispatch(setUser(infoUser));
+      }else{
+        dispatch(resetUser());
+      }
+    });
+  }, []);
+
   return (
     <>
       <Router>
---
+++ semana13/firechat/src/app/auth/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    logged: false,
+    uid: null,
+    photo: null,
+  },
+
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload.name;
+      state.uid = action.payload.uid;
+      state.photo = action.payload.photo;
+      state.logged = true;
+    },
+    resetUser: (state) => {
+      state.user = null;
+      state.logged = false;
+      state.uid = null;
+      state.photo = null;
+    },
+  },
+});
+
+export const { setUser, resetUser } = authSlice.actions;
+export const selectUser = (state) => state.auth.user;
+//cuando exportamos algo por defecto al importarlo le podemos poner un alias para que sea más fácil de leer el código
+export default authSlice.reducer;
