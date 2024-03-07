daf47ab30a68b06c3256271189a1fcbd21118257
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-03-06 20:29:09 -0500

    fix navbar

--- semana13/firechat/src/components/Nav.jsx
+++ semana13/firechat/src/components/Nav.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../app/auth/authSlice";
 import { OverlayTrigger, Popover } from "react-bootstrap";
+import { logoutUser } from "../services/authService";
 
 export default function Nav() {
+  const [showPopover, setShowPopover] = useState(false)
+
   const user = useSelector(selectUser);
-  console.log(user);
+  
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      alert("Sesión cerrada!")
+      setShowPopover(false)
+    } catch (error) {
+      alert("Error al salir")
+      setShowPopover(false)
+    }
+  }
 
   const popover = (
     <Popover id="popover-basic">
-      <Popover.Header as="h3">Menu usuario</Popover.Header>
+      <Popover.Header as="h3">
+        Menu usuario
+      </Popover.Header>
       <Popover.Body>
-        <span>Cerrar sesión</span>        
+        <button className="btn btn-sm btn-primary" onClick={handleLogout}>Cerrar sesión</button>
       </Popover.Body>
     </Popover>
   );
@@ -37,8 +53,11 @@ export default function Nav() {
                 trigger="click"
                 placement="bottom"
                 overlay={popover}
+                show={showPopover}
               >
-                <span className="text-white">{user.user}</span>
+                <span className="text-white" onClick={() => {setShowPopover(!showPopover)}}>
+                  {user.user}
+                </span>
               </OverlayTrigger>
             </>
           )}
